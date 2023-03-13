1e6bb4cf9725d67350d6be43875dd3f17f3cdbd6
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-03-13 16:25:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply  = num1 * num2 
+console.log(multiply)
+
+//
+const random = Math.floor(Math.random() * 10 )+1
+ console.log(random);
+
+const num3 = 14
+const num4 = 5
+const mod = num3 % num4
+console.log(mod);
+
+const numbers = [5, 10, 15, 20, 2, 8];
+const max = Math.max(...numbers);
+console.log(max); 
